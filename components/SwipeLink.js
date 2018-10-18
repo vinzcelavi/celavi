480589898bf17699b499d290cc951696620efb84
Vincent BIANCIOTTO <vincent@celavi.fr>
2018-10-18 23:04:01 +0200

    Refine hover effect

--- components/SwipeLink.js
+++ components/SwipeLink.js
@@ -1,27 +1,33 @@
 import React from 'react'
 import styled from 'styled-components'
-import { rgba } from 'polished'
 
 import FONTS from '../constants/fonts'
 import COLORS from '../constants/colors'
 
+const TRANS_FUNCTION = 'cubic-bezier(0.77, 0, 0.175, 1)'
+const TRANS_DURATION_IN = '0.35s'
+const TRANS_DURATION_OUT = '0.5s'
+const HOVER_WIDTH = '70%'
+
 const StyledSwipeLink = styled.a`
   position: relative;
   display: inline-flex;
-  margin: 0 -10px;
-  padding: 2px 10px;
+  margin: 0 -20px;
+  padding: 1px 20px;
   font-family: ${FONTS.PRIMARY};
   font-size: 24px;
   line-height: 1.5;
   font-weight: 700;
   text-decoration: none;
   color: ${COLORS.WHITE};
-  transition: all 0.35s ease;
+  overflow: hidden;
+  transition: all 0.35s ${TRANS_FUNCTION};
 
   span {
     z-index: 20;
     position: relative;
-    transition: all 0.35s ease;
+    text-shadow: 0 2px 35px ${COLORS.BLACK};
+    transition: all ${TRANS_DURATION_IN} ${TRANS_FUNCTION};
   }
 
   &::before,
@@ -31,42 +37,45 @@ const StyledSwipeLink = styled.a`
     position: absolute;
     bottom: 0;
     left: 0;
-    width: 80%;
+    width: 30%;
     height: 100%;
     background-color: ${COLORS.YELLOW};
-    opacity: 0;
   }
 
   &::before {
     z-index: 10;
-    transform: translateX(-60%);
-    transition: all 0s ease;
+    opacity: 1;
+    transform: translateX(-110%);
+    transition: all 0s ${TRANS_FUNCTION};
   }
 
   &::after {
     z-index: 5;
-    transform: translateX(40%);
-    transition: all 0.35s ease;
+    width: ${HOVER_WIDTH};
+    opacity: 1;
+    transform: translateX(190%);
+    transition: all ${TRANS_DURATION_OUT} ${TRANS_FUNCTION};
   }
 
   &:hover {
     span {
-      color: ${COLORS.WHITE};
+      transform: translateX(12px);
     }
 
     &::before,
     &::after {
+      width: ${HOVER_WIDTH};
       opacity: 1;
     }
 
     &::before {
-      transform: translateX(-30%);
-      transition: all 0.35s ease;
+      transform: translateX(0);
+      transition: all ${TRANS_DURATION_IN} ${TRANS_FUNCTION};
     }
 
     &::after {
-      transform: translateX(-30%);
-      transition: all 0s 0.35s ease;
+      transform: translateX(0);
+      transition: all 0s ${TRANS_DURATION_IN} ${TRANS_FUNCTION};
     }
   }
 `
