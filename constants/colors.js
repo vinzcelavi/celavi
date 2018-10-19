cfd27ee935f0dcde3be5b9c66a2e35ebd9c06c72
Vincent BIANCIOTTO <vincent@celavi.fr>
2018-10-19 15:11:31 +0200

    Darken yellow color

--- constants/colors.js
+++ constants/colors.js
@@ -2,7 +2,7 @@ const COLORS = {
   WHITE:        '#FFFFFF',
   BLACK:        '#000000',
   ANTHRACITE:   '#191C23',
-  YELLOW:       '#F1C40F',
+  YELLOW:       '#E3BC1F',
   BLUE:         '#54a0ff',
   GREEN:        '#2CAF90',
   GRAY:         '#888888',
