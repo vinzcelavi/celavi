14663be44e3c9b783761816c640052551bc161d2
Vincent BIANCIOTTO <vincent@celavi.fr>
2018-10-19 23:32:30 +0200

    Favicon

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,11 @@ export default class MyDocument extends Document {
           <title>Vincent Bianciotto | UI/UX Designer</title>
           {this.props.styleTags}
           <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+          <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+          <link rel="icon" href="/static/favicon.png" type="image/png" />
+          <link rel="icon" sizes="32x32" href="/static/favicon-32.png" type="image/png" />
+          <link rel="icon" sizes="64x64" href="/static/favicon-64.png" type="image/png" />
+          <link rel="icon" sizes="96x96" href="/static/favicon-96.png" type="image/png" />
           <link rel="stylesheet" href="/static/style.css" />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css" />
           <link href="https://fonts.googleapis.com/css?family=Mada:300,400,500,600,700|Overpass+Mono:300,400,600,700" rel="stylesheet" />
