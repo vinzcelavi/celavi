f0e70998edb6db5f982d23c0c09b1448bbf821ae
Vincent Bianciotto <vincent.bianciotto@main-gauche.com>
2020-05-04 09:55:45 +0200

    Inverse toggle

--- components/Toggle.js
+++ components/Toggle.js
@@ -18,8 +18,8 @@ const ToggleButton = styled.button`
 
 const Icon = styled.div`
   position: relative;
-  height: 1.6rem;
-  width: 1.6rem;
+  height: 1.5rem;
+  width: 1.5rem;
   border-radius: 50%;
   background: linear-gradient(25deg, #8d77ff, #93cdff);
   transition: all 0.25s linear;
@@ -27,21 +27,21 @@ const Icon = styled.div`
   &::before {
     content: '';
     position: absolute;
-    top: -1px;
-    right: -1px;
-    width: 1.4rem;
-    height: 1.4rem;
+    top: -3px;
+    right: -3px;
+    width: 90%;
+    height: 90%;
     background: ${({ theme }) => theme.main.body};
     border-radius: 50%;
     transform: translate(0, 0);
-    transition: all 0.5s ease;
+    transition: all 0.45s ease;
   }
 
   ${({ lightTheme }) =>
     lightTheme &&
     `
     background: linear-gradient(25deg, #ff3c71, #ff8815);
-    
+
     &::before {
       transform: translate(100%, -100%);
     }
